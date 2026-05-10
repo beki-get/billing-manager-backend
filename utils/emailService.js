2ecb9eaf81e762f597d11953d6ac104a63ea2512
Bereket <166899288+beki-get@users.noreply.github.com>
2026-05-10 17:15:55 +0300

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 
-// Example using Gmail SMTP
+//  using Gmail SMTP
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,      // your email
-        pass: process.env.EMAIL_PASS   // app password if Gmail
+        pass: process.env.EMAIL_PASS   // app password
     }
 });
 
