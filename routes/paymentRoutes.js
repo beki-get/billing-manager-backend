c3fa65079e2cd462cc93edfbca68cfda6fdd61a7
beki-get <bereketgetayea@gmail.com>
2026-06-09 19:01:03 +0300

    added the end points for payment and webhook

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,14 @@
-// /routes/paymentRoutes.js
+//
 import { Router } from 'express';
 const router = Router();
 import paymentController from '../controllers/paymentController.js';
 import auth from '../middlewares/auth.js';
+import {validateSignature} from '../middlewares/webhookMiddleware.js';
 
-// Create a Stripe payment intent for an invoice
-console.log('protect is:', typeof auth.protect);
 
-router.post('/stripe', auth.protect, paymentController.payInvoiceStripe);
+router.post('/pay-invoice/:invoiceId', paymentController.initializeInvoicePayment);
+router.get('/pay-invoice/:invoiceId', paymentController.initializeInvoicePayment);
+router.post('/webhook', validateSignature, paymentController.handleChapaWebhook);
+router.get('/payment-success', paymentController.handlePaymentSuccessView);
 
 export default router;
